4bbf2bea0f3aa17c743d6f92fdd81e89230db22e
Nils Thamm <hsv.forever3@web.de>
2018-06-15 20:29:54 +0200

    First hacky attempt

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 #include <algorithm>
 
 #include <gsl/gsl_vector.h>
@@ -28,22 +29,70 @@ using namespace std;
 
 class PCAlgorithm {
 
-  public:
-    uint nr_variables, level;
-    const int STRIDE = 1;
+public:
 
-    arma::Mat<double>_correlation;
-    Graph _graph;
+    void build_graph() {
+        _correlation.print(cout);
+        IndepTestGauss indepTest(_nr_variables,_correlation);
 
-    PCAlgorithm(int vars): _graph(nr_variables) {
-        nr_variables = vars;
-        level = 0;
-        _correlation = arma::Mat<double>(nr_variables, nr_variables, arma::fill::eye);
+        int level = 1;
+        std::unordered_set<int> not_done;
+        for (int i = 0; i < _nr_variables; ++i) not_done.insert(not_done.end(),i);
+
+        // we want to run as long as their are edges remaining to test on a higher level
+        while(not_done.size()) {
+            std::vector<pair<int,int> > edges_to_delete(0);
+            std::vector<int> nodes_to_delete(0);
+            // iterate over all edges to determine if they still can be tested on this level
+            for(auto it = not_done.begin(); it != not_done.end(); it++) {
+                auto current_node = *it;
+                auto adj = _graph.getNeighbours(*it);
+                // only do the independence testing if the node has enough neighbours do create a seperation set
+                if(adj.size()-1 >= level) {
+
+                    for(int j = 0; j < adj.size() && adj[j] < current_node; j++) {
+                        std::vector<int> S(adj);
+                        S.erase(S.begin()+j); // Y should not be in Z for X ⊥ Y | Z
+                        // generating subsets by iteratively combining one element with l-1 following elements until
+                        // this element was combined with all following elements
+                        std::vector<int> subset(S.begin()+v-level+1, S.begin() +v);
+                        for(int u = 0; u+level <= S.size(); u++) {
+                            for(int v=level; v < S.size(); v++) {
+                                subset.push_back(S[u]);
+                                auto p = indepTest.test(current_node,adj[j],subset);
+                                if((1-p) < _alpha) {
+                                    edges_to_delete.push_back({current_node,adj[j]});
+                                    _seperation_sets.push_back({{current_node,adj[j]}, subset});
+                                    goto endloop;
+                                }
+                            }
+                        }
+                        endloop:
+                        ;
+                    }
+                } else {
+                    // if they have not enough neighbors for this level, they won't on the following
+                    nodes_to_delete.push_back(current_node);
+                }
+            }
+
+            for(const auto edge : edges_to_delete) {
+                _graph.deleteEdge(edge.first, edge.second);
+            }
+            for(const auto node : nodes_to_delete) {
+                not_done.erase(node);
+            }
+            level++;
+        }
+    }
+
+    void print_graph() {
+        _graph.print();
     }
 
     void build_correlation_matrix(std::vector<std::vector<double>> &data) {
         int n = data[0].size();
-        rep(i, nr_variables) {
+        rep(i, _nr_variables) {
             rep(j, i) {
                 gsl_vector_const_view gsl_x = gsl_vector_const_view_array( &data[i][0], n);
                 gsl_vector_const_view gsl_y = gsl_vector_const_view_array( &data[j][0], n);
@@ -53,9 +102,24 @@ class PCAlgorithm {
                     n
                 );
                 _correlation(i,j) = _correlation(j,i) = pearson;
+                if(pearson < _alpha) {
+                    _graph.deleteEdge(i,j);
+                }
             }
         }
     }
+
+    PCAlgorithm(int vars, double alpha): _graph(vars), _alpha(alpha), _nr_variables(vars) {
+        _correlation = arma::Mat<double>(vars, vars, arma::fill::eye);
+    }
+
+protected:
+    const int STRIDE = 1;
+    int _nr_variables;
+    arma::Mat<double>_correlation;
+    Graph _graph;
+    double _alpha;
+    vector<pair<pair<int, int>, vector<int> > > _seperation_sets;
 
 };
 
@@ -87,14 +151,16 @@ int main(int argc, char* argv[])
     cin.tie(nullptr);
     cout.precision(10);
 
-
+    // kann man überlegen, ob man das nicht auch in die Klasse mit rein zieht
     auto data = read_data();
 
-    PCAlgorithm alg(data.size());
+    PCAlgorithm alg(data.size(), 0.1);
 
     alg.build_correlation_matrix(data);
 
-    alg._correlation.print(cout);
+    alg.build_graph();
+
+    alg.print_graph();
 
     // std::vector<uint> sep1 = {5};
     // std::vector<uint> sep2 = {3};
