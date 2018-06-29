ab8c6d5b62cb8133b1d3838c7d8f6bf75f83df49
philipp.bode <philipp.bode@student.hpi.de>
2018-06-29 17:29:24 +0200

    Watcher base.

---
+++ watcher.cpp
@@ -0,0 +1,42 @@
+#include <chrono>
+#include <thread>
+
+#include "watcher.h"
+
+Watcher::Watcher(
+    std::vector<int> *stats,
+    int max
+) :
+    _stats(stats),
+    _max(max)
+{}
+
+void Watcher::watch() {
+
+    std::chrono::seconds sec(1);
+
+    while(true) {
+        if (_max) {
+            int done = 0;
+            for(auto const tests: (*_stats)) {
+                done += tests;
+            }
+            std::cout << done << " out of " << _max << std::endl;
+            std::cout << (double) _max / (double) done << " p. done." << std::endl;
+            std::cout << "\r\r";
+        }
+
+        std::this_thread::sleep_for(sec);
+    }
+
+    // needs a mutex to guarantee thread safety
+    _done = true;
+}
+
+void Watcher::set_max(int new_max) {
+    _max = new_max;
+}
+
+bool Watcher::done() const {
+    return _done;
+}
