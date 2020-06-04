915a3961ad5c5e8b14daceabea71fab434bf65aa
github-action-benchmark <github@users.noreply.github.com>
2020-06-04 18:47:31 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for b2f7c12c6922a38fcca2d6cd5a03fcfb52c29730

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1591296451779,
+  "repoUrl": "https://github.com/philipp-bode/lock-free-pc",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "philipp.bode@student.hpi.de",
+            "name": "philipp.bode",
+            "username": "philipp-bode"
+          },
+          "committer": {
+            "email": "philipp.bode@student.hpi.de",
+            "name": "philipp.bode",
+            "username": "philipp-bode"
+          },
+          "distinct": true,
+          "id": "b2f7c12c6922a38fcca2d6cd5a03fcfb52c29730",
+          "message": "Adds github token.",
+          "timestamp": "2020-06-04T20:42:48+02:00",
+          "tree_id": "6fdf667f2073b26d68b7600634ca54873d74dd36",
+          "url": "https://github.com/philipp-bode/lock-free-pc/commit/b2f7c12c6922a38fcca2d6cd5a03fcfb52c29730"
+        },
+        "date": 1591296451362,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/regression/test_skeleton_performance.py::test_skeleton_performance",
+            "value": 0.15413389375920558,
+            "unit": "iter/sec",
+            "range": "stddev: 0.022303872286031683",
+            "extra": "mean: 6.4878656836000115 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
