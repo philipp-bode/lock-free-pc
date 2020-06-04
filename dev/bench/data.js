5934100bcfd855b9ad355e55fd43ea36dac94cae
github-action-benchmark <github@users.noreply.github.com>
2020-06-04 19:08:51 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 899eea2f7205f2f9381d017b0c510e1fe897f495

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1591296451779,
+  "lastUpdate": 1591297731650,
   "repoUrl": "https://github.com/philipp-bode/lock-free-pc",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.022303872286031683",
             "extra": "mean: 6.4878656836000115 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "philipp.bode@student.hpi.de",
+            "name": "philipp.bode",
+            "username": "philipp-bode"
+          },
+          "committer": {
+            "email": "philipp.bode@student.hpi.de",
+            "name": "philipp.bode",
+            "username": "philipp-bode"
+          },
+          "distinct": true,
+          "id": "899eea2f7205f2f9381d017b0c510e1fe897f495",
+          "message": "Test alert threshold.",
+          "timestamp": "2020-06-04T21:03:23+02:00",
+          "tree_id": "c49398994fd01e85d5d1cd585e3b33883c893e61",
+          "url": "https://github.com/philipp-bode/lock-free-pc/commit/899eea2f7205f2f9381d017b0c510e1fe897f495"
+        },
+        "date": 1591297731269,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/regression/test_skeleton_performance.py::test_skeleton_performance",
+            "value": 0.039943554900109424,
+            "unit": "iter/sec",
+            "range": "stddev: 0.056513863169231376",
+            "extra": "mean: 25.0353280398 sec\nrounds: 5"
           }
         ]
       }
